504f4c4597b6b1bf41fe43e84bb5b9e3958c6430
Pierre-Luc Babin <plbabin@gmail.com>
2015-05-05 15:15:03 +0000

    Add base file for product cell

---
+++ app/scripts/components/cells/product_cell.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ProductCell extends React.Component {
+  
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <li></li>
+    );
+  }
+                                     
+}
+
+ProductCell.propTypes = {
+  
+};
+
+export default ProductCell;
