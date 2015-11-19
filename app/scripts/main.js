2692e8096cb902b9f3228aaca2f942775d87f2c9
Pierre-Luc Babin <plbabin@gmail.com>
2015-11-19 20:30:52 +0000

    Add redux router

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,13 @@
 import React                from 'react'
 import { render }           from 'react-dom'
-import { Router, Route }    from 'react-router';
+
+import { Route }            from 'react-router';
+
+import { reduxReactRouter, 
+         routerStateReducer, 
+         ReduxRouter }      from 'redux-router';
+
+import { createHistory }    from 'history';
 import routes               from './routes';
 
 import { Provider }         from 'react-redux';
@@ -9,20 +16,33 @@ import promiseMiddleware    from './lib/promiseMiddleware';
 import createLogger         from 'redux-logger';
 
 import { createStore,
+         compose,
          combineReducers,
          applyMiddleware }  from 'redux';
 
 
 //const history = createBrowserHistory();
 const logger = createLogger();
-const reducer = combineReducers(reducers);
-//let store = createStore(reducer);
-const store   = applyMiddleware(promiseMiddleware, logger)(createStore)(reducer);
+
+const reducer = combineReducers(
+  Object.assign({
+    router: routerStateReducer
+  }, reducers)
+);
+
+const store = compose(
+  applyMiddleware(promiseMiddleware, logger),
+  reduxReactRouter({
+    routes,
+    createHistory
+  })
+  //devTools()
+)(createStore)(reducer);
 
 //history={history}
 render(
   <Provider store={store}>
-    <Router children={routes} />
+    <ReduxRouter children={routes} />
   </Provider>,
   document.getElementById('content') 
 );
